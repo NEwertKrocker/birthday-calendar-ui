e4a5733e613cb2596ab5ab1cf7d8ce79d2b74a5c
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-12-15 11:55:38 -0500

    Add Months component and apiCalls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,23 @@
+
+import React, { Component } from 'react';
 import './App.css';
 import { months } from '../../months_data';
+import { getBirthdays } from '../apiCalls'
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      birthdays: []
+    }
+  }
 
+  componentDidMount = () => {
+    getBirthdays()
+    .then(data => {this.setState({ birthdays: data })})
+  }
+
+  render () {
   return (
     <div className="App">
       <h1>Birthdays</h1>
@@ -10,10 +25,11 @@ function App() {
 
       </div>
       <div className='bday-container'>
-
+        <Months birthdays={this.state.birthdays}/>
       </div>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/Components/Months/Months.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Months.css'
+import months from './months_data'
+
+const Months = ({ birthdays }) => {
+
+  const months = birthdays.map((birthday))
+
+  return (
+    <div>
+      <article>
+
+      </article>
+    </div>
+  )
+}
